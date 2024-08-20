e9e36896d21c0bc65121935ada59375617f0a629
temirovv <temirovv21@gmail.com>
2024-08-20 12:36:09 +0500

    some tests

--- users/models.py
+++ users/models.py
@@ -1,4 +1,6 @@
-from django.db.models import CharField, BooleanField, TextChoices
+from django.utils.translation import gettext_lazy as _
+
+from django.db.models import CharField, BooleanField, TextChoices, EmailField
 from django.contrib.auth.models import AbstractUser
 
 
@@ -7,7 +9,11 @@ class User(AbstractUser):
         MALE = 'male', 'Male'
         FEMALE = 'female', 'Female'
 
-    telegram_id = CharField(max_length=150, unique=True, null=True, blank=True)
+    first_name = CharField(_("first name"), max_length=150, null=True, blank=True)
+    last_name = CharField(_("last name"), max_length=150, null=True, blank=True)
+    email = EmailField(_("email address"), null=True, blank=True)
+    telegram_id = CharField(max_length=150, unique=True)
+
     is_verified = BooleanField(default=False)
     phone_number = CharField(max_length=30, null=True, blank=True)
     gender = CharField(max_length=10, choices=GenderChoices.choices, null=True, blank=True)
